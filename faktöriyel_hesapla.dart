53c46eaaea51ab04b411d4b17e69006461978846
Elif ÇALIŞ <101409313+elfrkn@users.noreply.github.com>
2022-03-28 23:45:42 +0300

    Create faktöriyel_hesapla.dart
    Faktoriyel hesaplamada oluşturulan classı çağırma

---
+++ faktöriyel_hesapla.dart
@@ -0,0 +1,10 @@
+import 'package:nesne_tabanli_programlama/faktoriyel.dart';
+
+void main(){
+
+  var f= faktoriyel();
+    int faktoriyelSonuc=f.hesap(5);
+    print("5 sayısının faktöriyeli: $faktoriyelSonuc");
+
+
+  }

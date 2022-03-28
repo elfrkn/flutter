23396bf7ee23ff1f5e2a8e43cd57299f8f95894a
Elif ÇALIŞ <101409313+elfrkn@users.noreply.github.com>
2022-03-28 19:55:20 +0300

    Create fahrenheit_main.dart
    temperature conversion

---
+++ fahrenheit_main.dart
@@ -0,0 +1,11 @@
+import 'package:nesne_tabanli_programlama/fahrenheit.dart';
+
+void main(){
+
+  var f=Fahren();
+  double donusum_sonuc=f.donustur(30);
+  print("Fahrenheit olarak sıcaklık derecesi: $donusum_sonuc");
+
+
+
+}

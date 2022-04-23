a9bd821f561f74332ff5874d93809703f660ea90
Elif ÇALIŞ <101409313+elfrkn@users.noreply.github.com>
2022-04-23 10:39:27 +0300

    İç açı Toplama
    Main sayfası.classı çağırdığımız.
    Girilen kenar sayısına göre iç açılar toplamını bulma.

---
+++ ic_aci_toplam_main.dart
@@ -0,0 +1,12 @@
+import 'package:nesne_tabanli_programlama/ic_aci_toplam.dart';
+
+void main(){
+
+
+  var hesapla=icacitoplam();
+  int sonuc=hesapla.aciToplam(5);
+  print("İç açı toplamı:$sonuc");
+
+
+
+}

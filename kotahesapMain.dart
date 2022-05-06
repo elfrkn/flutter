cb82c47cdba99842f7d1aec8239f7064f05588fa
Elif ÇALIŞ <101409313+elfrkn@users.noreply.github.com>
2022-05-06 16:59:07 +0300

    Kota Hesap Main

---
+++ kotahesapMain.dart
@@ -0,0 +1,8 @@
+import 'package:nesne_tabanli_programlama/kotahesap.dart';
+
+void main(){
+
+  var sonuc=kotaHesap();
+  int hesapla=sonuc.kotaHesapla(56);
+  print("İnternet Ücretiniz:$hesapla");
+}
